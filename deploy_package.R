49b382af9cfd423249010799a367be138d79b574
Van Hung Tran <tranhungydhcm@gmail.com>
2025-09-29 16:05:55 +0200

    Update: After fixing upstream configuration

--- deploy_package.R
+++ deploy_package.R
@@ -1,8 +1,2 @@
 source(here::here("deploy_mestools.R"))
-deploy_mestools()
-
-
-
-
-source("deploy_mestools.R")
-deploy_mestools_quick("Fix: Update utility functions and documentation")
+check_mestools_status()
